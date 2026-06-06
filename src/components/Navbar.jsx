1c2dd94cbc5081ec6868c4afc7b805cb1a71a35f
MrudulTarade <149925407+MrudulTarade@users.noreply.github.com>
2026-06-06 20:30:50 +0200

    features/navbar
    Scroll effects and fonts.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,97 @@
 import "./Navbar.css";
 import profile from "../assets/images/profile.jpg";
+import {
+  motion,
+  useScroll,
+  useTransform,
+  useSpring,
+  useMotionTemplate
+} from "framer-motion";
+
 
 function Navbar() {
+  const { scrollY } = useScroll();
+  const profileSize = useTransform(
+  scrollY,
+  [0, 200],
+  [150, 85]
+);
+
+const navScale = useTransform(
+  scrollY,
+  [0, 200],
+  [1, 0.85]
+);
+
+const nameOpacity = useTransform(
+  scrollY,
+  [50, 200],
+  [0, 1]
+);
+  const smoothProfileSize =
+  useSpring(profileSize, {
+    stiffness: 120,
+    damping: 25
+  });
+  const smoothNavScale = useSpring(navScale, {
+    stiffness: 120,
+    damping: 25
+  });
+  const smoothNameOpacity = useSpring(nameOpacity, {
+    stiffness: 120,
+    damping: 25
+  });
+  const nameBlur = useTransform(
+  scrollY,
+  [50, 200],
+  [10, 0]
+);
+const blurFilter =
+  useMotionTemplate`blur(${nameBlur}px)`;
+const navX = useTransform(
+  scrollY,
+  [0, 200],
+  [0, 400]
+);
+const smoothNavX =
+  useSpring(navX, {
+    stiffness: 120,
+    damping: 25
+  });
   return (
 <nav className="navbar-wrapper">
 
-  <div className="profile-container">
-    <img
-      src={profile}
-      alt="Profile"
-      className="profile-pic"
-    />
-  </div>
+  <motion.div
+  className="profile-container"
+  >
+    <motion.img
+  src={profile}
+  alt="Profile"
+  className="profile-pic"
+  style={{
+    width: smoothProfileSize,
+    height: smoothProfileSize
+  }}
+   />
+  </motion.div>
 
-  <div className="name-container">
-    <h2>Your Name</h2>
-  </div>
+  <motion.div
+  className="name-container"
+  style={{
+    opacity: smoothNameOpacity,
+    filter: blurFilter
+  }}
+  >
+    <h2>Hi, I'm Mrudul Tarade</h2>
+  </motion.div>
 
-  <div className="navbar-container">
+  <motion.div
+  className="navbar-container"
+  style={{
+    scale: smoothNavScale,
+    x: smoothNavX
+  }}
+  >
 
     <ul className="nav-links">
 
@@ -31,7 +105,7 @@ function Navbar() {
 
     </ul>
 
-  </div>
+  </motion.div>
 
 </nav>
   );
