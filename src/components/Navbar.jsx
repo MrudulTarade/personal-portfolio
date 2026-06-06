ce0f896c47ff4538956a08dccfb098c6620c6cf1
MrudulTarade <149925407+MrudulTarade@users.noreply.github.com>
2026-06-06 13:41:22 +0200

    foundation/navbar
    Adjustments to the previous setup. Change in profile picture and ratio changes for further effects.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,41 +3,37 @@ import profile from "../assets/images/profile.jpg";
 
 function Navbar() {
   return (
-    <nav className="navbar">
+<nav className="navbar-wrapper">
 
-      <div className="profile-wrapper">
-        <img
-          src={profile}
-          alt="Profile"
-          className="profile-pic"
-        />
-      </div>
+  <div className="profile-container">
+    <img
+      src={profile}
+      alt="Profile"
+      className="profile-pic"
+    />
+  </div>
 
-      <div className="navbar-container">
+  <div className="name-container">
+    <h2>Your Name</h2>
+  </div>
 
-        <ul className="nav-links">
+  <div className="navbar-container">
 
-          <li>
-            <a href="#home">Home</a>
-          </li>
+    <ul className="nav-links">
 
-          <li>
-            <a href="#about">About</a>
-          </li>
+      <li><a href="#home">Home</a></li>
 
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
+      <li><a href="#about">About</a></li>
 
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
+      <li><a href="#projects">Projects</a></li>
 
-        </ul>
+      <li><a href="#contact">Contact</a></li>
 
-      </div>
+    </ul>
 
-    </nav>
+  </div>
+
+</nav>
   );
 }
 
