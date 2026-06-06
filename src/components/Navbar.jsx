d062cffa433ce9e42f15b12d546390bae7f754fc
MrudulTarade <149925407+MrudulTarade@users.noreply.github.com>
2026-06-06 13:10:08 +0200

    foundation/Navbar
    Initial setup of the navbar. Changed the positions of the elements of the navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,42 @@
+import "./Navbar.css";
 import profile from "../assets/images/profile.jpg";
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="logo-section">
-        <img src={profile} alt="profile" />
 
-        <span>Your Name</span>
+      <div className="profile-wrapper">
+        <img
+          src={profile}
+          alt="Profile"
+          className="profile-pic"
+        />
       </div>
 
-      <ul>
-        <li>
-          <a href="#about">About</a>
-        </li>
+      <div className="navbar-container">
 
-        <li>
-          <a href="#projects">Projects</a>
-        </li>
+        <ul className="nav-links">
 
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+
+          <li>
+            <a href="#about">About</a>
+          </li>
+
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+
+        </ul>
+
+      </div>
+
     </nav>
   );
 }
